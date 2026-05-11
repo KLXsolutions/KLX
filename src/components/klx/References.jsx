68737898a6a1d491792b39629fe2fdab46485db2
KLXsolutions <nx84pgvr4d@privaterelay.appleid.com>
2026-05-11 22:22:47 +0200

    Sync src/components/klx/References.jsx

--- src/components/klx/References.jsx
+++ src/components/klx/References.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
-import { useI18n } from '../../lib/i18n.jsx';
-import SectionHeader from './SectionHeader.jsx';
-import ProjectGallery from './ProjectGallery.jsx';
+import { useI18n } from '@/lib/i18n.jsx';
+import SectionHeader from './SectionHeader';
+import ProjectGallery from './ProjectGallery';
 
 export default function References() {
-  var ctx = useI18n();
-  var r = ctx.t.refs;
+  const { t } = useI18n();
+  const r = t.refs;
+
   return (
-    <section id="references" style={{ padding:'96px 24px' }}>
-      <div style={{ maxWidth:'80rem', margin:'0 auto' }}>
+    <section id="references" className="py-24 px-6 md:px-8">
+      <div className="max-w-7xl mx-auto">
         <SectionHeader title={r.title} titleAccent={r.titleAccent} sub={r.sub} />
         <ProjectGallery />
       </div>

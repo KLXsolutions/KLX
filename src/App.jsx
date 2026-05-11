31acef9d429e1b0c35e33f2bb18edd1def2411b9
KLXsolutions <nx84pgvr4d@privaterelay.appleid.com>
2026-05-11 20:41:29 +0200

    Fix: use HashRouter for GitHub Pages compatibility

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,24 @@
-import React from 'react';
-import Home from './pages/Home.jsx';
+import { Toaster } from "@/components/ui/toaster"
+import { QueryClientProvider } from '@tanstack/react-query'
+import { queryClientInstance } from '@/lib/query-client'
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import PageNotFound from './lib/PageNotFound';
+import Home from '@/pages/Home';
+import GitHubRepos from '@/pages/GitHubRepos';
 
-export default function App() {
-  return <Home />;
+function App() {
+  return (
+    <QueryClientProvider client={queryClientInstance}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/github" element={<GitHubRepos />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </Router>
+      <Toaster />
+    </QueryClientProvider>
+  )
 }
+
+export default App

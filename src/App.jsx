ae766390ca0d9e682b6b16dc22957f5323fb14ca
KLXsolutions <nx84pgvr4d@privaterelay.appleid.com>
2026-05-11 22:26:34 +0200

    Sync src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
-import React from 'react';
+import { Toaster } from "@/components/ui/toaster"
+import { QueryClientProvider } from '@tanstack/react-query'
+import { queryClientInstance } from '@/lib/query-client'
 import { HashRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './pages/Home.jsx';
+import PageNotFound from './lib/PageNotFound';
+import Home from '@/pages/Home';
+import GitHubRepos from '@/pages/GitHubRepos';
+// Add page imports here
 
-export default function App() {
+function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </Router>
-  );
+    <QueryClientProvider client={queryClientInstance}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/github" element={<GitHubRepos />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </Router>
+      <Toaster />
+    </QueryClientProvider>
+  )
 }
+
+export default App
